7184811e44de3af413209320883941197eda2e15
apples1309 <apples1309@gmail.com>
2022-08-05 17:06:19 +0900

    init web-client

---
+++ web-client/vue.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = {
+  outputDir: '../backend/src/main/resources/static',
+  devServer: {
+    port: 9999,
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:8080'
+      }
+    }
+  }
+}
